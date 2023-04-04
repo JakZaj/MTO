30a869d14089322f0d6fb9a2dfee9d68c4a4aec0
JakZaj <jakubzajac13@gmail.com>
2023-04-04 17:32:41 +0200

    Dodanie funkcji zmniejszającej liczbę

--- lab5/main.java
+++ lab5/main.java
@@ -4,7 +4,7 @@ import java.io.*;
 class lab0 {
 	public static void my_printf(String format_string, String param){
 		for(int i=0;i<format_string.length();i++){
-			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
+			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'g')){
 				System.out.print(param);
 				i++;
 			}else{
@@ -13,6 +13,20 @@ class lab0 {
 		}
 		System.out.println("");
 	}
+
+	public String reductNumber(String strNumber){
+
+		String retStr = "";
+		for(int i=0;i < strNumber.length();i++){
+			if(strNumber.charAt(i) == '0'){
+				retStr += '9';
+			}else{
+				retStr += (char)(strNumber.charAt(i) - 1)
+			}
+		}
+		return retStr;
+	}
+
 	public static void main(String[] args) throws IOException {
 		//System.out.println("Hello, World!"); 
 		BufferedReader bufferReader=new BufferedReader(new InputStreamReader(System.in));
