ed6129834d34c23b6751efae37e8db44a9a1361f
JakZaj <jakubzajac13@gmail.com>
2023-04-18 17:30:04 +0200

    Zmiana nazwa funkcji oraz jej działania

--- lab6/main.java
+++ lab6/main.java
@@ -5,13 +5,13 @@ class lab0 {
 	public static void my_printf(String format_string, String param){
 		for(int i=0;i<format_string.length();i++){
 			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'g')){
-				System.out.print(reductNumber(param));
+				System.out.print(doNewNumber(param));
 				i++;
 			}else if((format_string.charAt(i) == '#') && (Character.isDigit(format_string.charAt(i+1))) && (format_string.charAt(i+2) == 'g')){
 				if((int)(format_string.charAt(i+1) - '0') < param.length())
-					System.out.print(reductNumber(param).substring(0, (int)(format_string.charAt(i+1) - '0')));
+					System.out.print(doNewNumber(param).substring(0, (int)(format_string.charAt(i+1) - '0')));
 				else{
-					System.out.print(reductNumber(param));
+					System.out.print(doNewNumber(param));
 				}
 				i+=2;
 			}
@@ -22,15 +22,18 @@ class lab0 {
 		System.out.println("");
 	}
 
-	public static String reductNumber(String strNumber){
+	public static String doNewNumber(String strNumber){
 
 		String retStr = "";
+		int number = 0;
 		for(int i=0;i < strNumber.length();i++){
-			if(strNumber.charAt(i) == '0'){
-				retStr += '9';
-			}else{
-				retStr += (char)(strNumber.charAt(i) - 1);
-			}
+			number = (int)strNumber.charAt(i);
+			number = (number * 9 + 1) % 10;
+			if(number == 0)
+				continue;
+
+			retStr += (char)(number);
+
 		}
 		return retStr;
 	}
