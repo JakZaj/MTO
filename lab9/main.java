5b2bad135aa6aa621bb65c7763293137a09fef29
JakZaj <jakubzajac13@gmail.com>
2023-05-23 17:30:44 +0200

    Praca nad my_printf

--- lab9/main.java
+++ lab9/main.java
@@ -4,30 +4,18 @@ import java.math.BigInteger;
 class lab0 {
 	public static void my_printf(String format_string, String param){
 		for(int i=0;i<format_string.length();i++){
-			if((format_string.charAt(i) == '#') && (format_string.charAt(i + 1) == 'j')) {
-				BigInteger bigInteger = new BigInteger(param);
-				String paramToPrint = bigInteger.toString(16).replace('0', 'o').replace('a', 'g').replace('b', 'h').replace('c', 'i').replace('d', 'j').replace('e', 'k').replace('f', 'l');
-				System.out.print(paramToPrint);
-				i++;
-			} else if((format_string.charAt(i) == '#') && (format_string.charAt(i + 1) == '.') && (Character.isDigit(format_string.charAt(i + 2))) && (format_string.charAt(i + 3) == 'j')){
-				BigInteger bigInteger = new BigInteger(param);
-				String paramToPrint = bigInteger.toString(16).replace('0','o').replace('a', 'g').replace('b', 'h').replace('c', 'i').replace('d', 'j').replace('e', 'k').replace('f', 'l');
+			if((format_string.charAt(i) == '#') && (format_string.charAt(i + 1) == '.') && (Character.isDigit(format_string.charAt(i + 2)))){
+				int formatStringPosition = i + 3;
+				int valueofZ = format_string.charAt(i + 2) - '0';
 
-				int z=format_string.charAt(i + 2) - '0';
-				
-				if(z > paramToPrint.length())
-					for(int k = 0; k < z - paramToPrint.length(); k++) {
-						if (paramToPrint.charAt(0) == '-') {
-							paramToPrint = "-o" + paramToPrint.substring(1);
-							k--;
-						} else {
-							paramToPrint = "o" + paramToPrint;
-						}
-					}
+				while(Character.isDigit(format_string.charAt(formatStringPosition))){
+					valueofZ *= 10;
+					valueofZ += format_string.charAt(formatStringPosition) - '0';
+					formatStringPosition++;
+				}
+				if(format_string.charAt(formatStringPosition) == 'h')
 
-				System.out.print(paramToPrint);
-
-				i+=3;
+					i+=3;
 			}else{
 				System.out.print(format_string.charAt(i));
 			}
