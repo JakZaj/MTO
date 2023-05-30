81fcae1c71635175c90d06a8b13b7e7bbd9161cb
JakZaj <jakubzajac13@gmail.com>
2023-05-30 17:34:11 +0200

    Praca nad my_printf

--- lab10/main.java
+++ lab10/main.java
@@ -4,30 +4,19 @@ import java.math.BigInteger;
 class lab0 {
 	public static void my_printf(String format_string, String param){
 		for(int i=0;i<format_string.length();i++){
-			if((format_string.charAt(i) == '#') && (format_string.charAt(i + 1) == 'j')) {
+			if((format_string.charAt(i) == '#') && (format_string.charAt(i + 1) == 'a')) {
+
 				BigInteger bigInteger = new BigInteger(param);
+
+				BigInteger valueF = bigInteger.multiply(BigInteger.valueOf(2));
 				String paramToPrint = bigInteger.toString(16).replace('0', 'o').replace('a', 'g').replace('b', 'h').replace('c', 'i').replace('d', 'j').replace('e', 'k').replace('f', 'l');
 				System.out.print(paramToPrint);
 				i++;
-			} else if((format_string.charAt(i) == '#') && (format_string.charAt(i + 1) == '.') && (Character.isDigit(format_string.charAt(i + 2))) && (format_string.charAt(i + 3) == 'j')){
-				BigInteger bigInteger = new BigInteger(param);
-				String paramToPrint = bigInteger.toString(16).replace('0','o').replace('a', 'g').replace('b', 'h').replace('c', 'i').replace('d', 'j').replace('e', 'k').replace('f', 'l');
 
-				int z=format_string.charAt(i + 2) - '0';
-				
-				if(z > paramToPrint.length())
-					for(int k = 0; k < z - paramToPrint.length(); k++) {
-						if (paramToPrint.charAt(0) == '-') {
-							paramToPrint = "-o" + paramToPrint.substring(1);
-							k--;
-						} else {
-							paramToPrint = "o" + paramToPrint;
-						}
-					}
-
+				paramToPrint = bigInteger.toString(16);
 				System.out.print(paramToPrint);
 
-				i+=3;
+				i++;
 			}else{
 				System.out.print(format_string.charAt(i));
 			}
